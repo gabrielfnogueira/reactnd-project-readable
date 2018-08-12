a27f12b2e7253067118810319929bb7a69b82fbe
Gabriel Nogueira <gfnogueira@gmail.com>
2018-08-12 17:43:19 -0300

    feat: Add react-router to handle app routes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,9 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './components/App';
-import './index.css';
 import store from './redux/store';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <Provider store={store}>
@@ -12,4 +10,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
-registerServiceWorker();
