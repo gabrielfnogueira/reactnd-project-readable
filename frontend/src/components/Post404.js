23e5abbfec1735e97a9347bc6659249ac4476298
Gabriel Nogueira <gfnogueira@gmail.com>
2018-08-15 20:27:58 -0300

    refactor: Add link to home on 404 page

--- frontend/src/components/Post404.js
+++ frontend/src/components/Post404.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 const Wrapper = styled.div``;
 
+const Title = styled.h1`
+  margin-bottom: 2rem;
+`;
+
+const HomeLink = styled(Link)`
+  font-weight: bold;
+  text-decoration: underline;
+`;
+
 const Post404 = () => {
-  return <Wrapper>Post not found :(</Wrapper>;
+  return (
+    <Wrapper>
+      <Title>Post not found :(</Title>
+      <p>
+        Why don't you go back <HomeLink to="/">home</HomeLink> and check the other posts?
+      </p>
+    </Wrapper>
+  );
 };
 
 export default Post404;
